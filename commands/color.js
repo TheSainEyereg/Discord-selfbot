df6a14520c0a5fab29f88da660cc962bd1847ad3
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2021-08-24 16:46:42 +0300

    FIxed color command

--- commands/color.js
+++ commands/color.js
@@ -1,13 +1,13 @@
-const { RichEmbed } = require("discord.js")
+const { RichEmbed } = require("discord.js");
+const Messages = require("../core/Messages");
 
 module.exports = {
     name: "color",
     args: true,
     aliases: ["hex"],
     execute(message, args) {
-        args[0]=args[0].replace("#", "");
-        const color = parseInt(args[0]) ? args[0] : args[0].length == 6 ? parseInt(args[0], 16) : parseInt(args[0], 16)**2+parseInt(args[0], 16)*2;
-        console.log(color);
+        const color = parseInt(args[0]) ? args[0] : args[0].replace("#", "").length == 6 ? parseInt(args[0].replace("#", ""), 16) : parseInt(args[0].replace("#", ""), 16)**2+parseInt(args[0].replace("#", ""), 16)*2;
+        if (!color) return Messages.warning(message, "That is not a color!", {timeout: 1000});
         message.edit(new RichEmbed({
             color: color,
             title: args[0]
