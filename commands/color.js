01599284dead4f68678c15b47cef3ac248bbca37
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2021-08-24 16:48:20 +0300

    Fixed 16777215 error

--- commands/color.js
+++ commands/color.js
@@ -7,7 +7,7 @@ module.exports = {
     aliases: ["hex"],
     execute(message, args) {
         const color = parseInt(args[0]) ? args[0] : args[0].replace("#", "").length == 6 ? parseInt(args[0].replace("#", ""), 16) : parseInt(args[0].replace("#", ""), 16)**2+parseInt(args[0].replace("#", ""), 16)*2;
-        if (!color) return Messages.warning(message, "That is not a color!", {timeout: 1000});
+        if (!color || color>16777215) return Messages.warning(message, "That is not a color!", {timeout: 1000});
         message.edit(new RichEmbed({
             color: color,
             title: args[0]
