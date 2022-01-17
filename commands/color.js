76219b73a35b8f088a4db6e817ddfb3a4289abe4
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2022-01-18 01:48:05 +0300

    Fixed hex convertation

--- commands/color.js
+++ commands/color.js
@@ -6,7 +6,8 @@ module.exports = {
 	args: true,
 	aliases: ["hex"],
 	execute(message, args) {
-		const color = parseInt(args[0]) ? args[0] : args[0].replace("#", "").length == 6 ? parseInt(args[0].replace("#", ""), 16) : parseInt(args[0].replace("#", ""), 16)**2+parseInt(args[0].replace("#", ""), 16)*2;
+		const string = args[0].slice(1);
+		const color = parseInt(args[0]) || (string.length === 6 ? parseInt(string, 16) : parseInt(string.split("").map(v => v+v).join(""), 16));
 		if (!color || color>16777215) return Messages.warning(message, "That is not a color!", {timeout: 1000});
 		message.edit(new RichEmbed({
 			color: color,
--- integrations/hex.js
+++ integrations/hex.js
@@ -4,7 +4,7 @@ module.exports = {
 	regexp: /^#(?:[0-9a-fA-F]{3}){1,2}$/gi,
 	execute(message) {
 		const string = message.content.slice(1);
-		const color = string.length == 6 ? parseInt(string, 16) : parseInt(string, 16)**2+parseInt(string, 16)*2;
+		const color = string.length === 6 ? parseInt(string, 16) : parseInt(string.split("").map(v => v+v).join(""), 16);
 		message.edit(new RichEmbed({
 			color: color,
 			title: "#"+string
