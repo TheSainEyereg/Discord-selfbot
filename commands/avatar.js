c24e7290e9d4c06ae1ecaf473480debf7e221d7a
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2022-01-25 15:18:05 +0300

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,11 @@
+const Messages = require("../core/Messages");
+
+module.exports = {
+	name: "avatar",
+	args: false,
+	execute(message, args) {
+		const user = message.mentions.users.first() || message.client.users.find(u => u.username.toLowerCase() == args.join(" ").toLowerCase() || u.id == args[0]) || message.author;
+		if (!user) return Messages.error(message, "User was not found.");
+		Messages.textRegular(message, user.avatarURL+"?size=1024");
+	}
+};
