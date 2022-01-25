4e8efaf45bdf6ceb6e11edce962565f77e9d284a
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2022-01-25 15:18:07 +0300

    Create ascii.js

---
+++ commands/ascii.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+const Messages = require("../core/Messages");
+
+module.exports = {
+	name: "ascii",
+	args: true,
+	execute(message, args) {
+		axios.get(`https://artii.herokuapp.com/make?text=${args.join(" ")}`)
+		.then(res => {
+			Messages.textRegular(message, `\`\`\`${res.data}\`\`\``);
+		})
+		.catch(e => {
+			Messages.error(message, "Something went wrong!", {description: e, timeout: 2500});
+		});
+	}
+};
