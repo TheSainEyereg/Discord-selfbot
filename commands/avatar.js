c26d0e524bc67534b60deaa0fb9486bfc716af77
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2022-01-28 15:39:45 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -6,6 +6,6 @@ module.exports = {
 	execute(message, args) {
 		const user = message.mentions.users.first() || message.client.users.find(u => u.username.toLowerCase() == args.join(" ").toLowerCase() || u.id == args[0]) || message.author;
 		if (!user) return Messages.error(message, "User was not found.");
-		Messages.textRegular(message, user.avatarURL+"?size=1024");
+		Messages.textRegular(message, user.avatarURL);
 	}
 };
