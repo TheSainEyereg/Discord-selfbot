5175017f5718207dd82cf6b607c828b41a679eec
TheSainEyereg <oleglogwinow@gmail.com>
2022-08-05 02:29:32 +0300

    Fixed

--- commands/roulette.js
+++ commands/roulette.js
@@ -3,33 +3,33 @@ const Messages = require("../core/Messages");
 module.exports = {
 	name: "roulette",
 	aliases: ["russian"],
+	
+	started: false,
 	async execute(message, args) {
-		if (!global.roulette) global.roulette = {started: false};
-
 		if (args[0] == "stop") {
 			Messages.completed(message, "Game stoped!");
-			return global.roulette.started = false;
+			return this.started = false;
 		}
 		if (args[0] == "spin") {
 			Messages.warning(message, "Spining the wheel!");
-			return global.roulette.current = Math.floor(Math.random()*6);
+			return this.current = Math.floor(Math.random()*6);
 		}
 
-		if (!global.roulette.started) {
-			global.roulette.started = true;
-			global.roulette.selected = 6; //Math.floor(Math.random()*6);
-			global.roulette.current = Math.floor(Math.random()*6);
+		if (!this.started) {
+			this.started = true;
+			this.current = Math.floor(Math.random()*6);
+			console.log("Selected: ", this.current);
 			return Messages.warning(message, "Game started!");
 		}
 
-		if (global.roulette.current == global.roulette.selected) {
+		if (this.current === 5) {
 			return Messages.error(message, `${message.author.tag} got his brains out!`, {callback: embed => {
 				message.edit(embed).then(_=>{
-					global.roulette.started = false;
+					this.started = false;
 				}).catch()
 			}})
 		}
-		global.roulette.current++;
+		this.current++;
 		Messages.regular(message, "Nice try!")
 	}
 }
